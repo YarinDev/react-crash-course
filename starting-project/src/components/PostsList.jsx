089eb389ca69fb6335b89cdb35d338546b444c6d
Yarin <yarin.software@gmail.com>
2024-01-25 19:48:43 +0200

    Added form buttons.

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
-function PostsLists() {
+function PostsLists({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
-        <Post author="Yarin" body="React is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Inbar" body="JS is awesome!" />
       </ul>
     </>
@@ -15,3 +36,4 @@ function PostsLists() {
 }
 
 export default PostsLists;
+
